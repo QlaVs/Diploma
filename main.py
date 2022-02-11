82816ea49e9624d7dacc3a624ad82e460d185055
petrulya_2000@mail.ru <qLQUNashY8AucRk>
2022-02-11 21:12:32 +0300

    Get all thing working with one tab

--- main.py
+++ main.py
@@ -1,55 +1,71 @@
+"""
+TODO:
+    1. Log Analyzer
+    2. Visualize log data to expert
+"""
+
 import json
 import time
+import logging
+import threading
 from read_screen import Reader
 from selenium import webdriver
 
+logging.getLogger().setLevel(logging.INFO)
+# logging.basicConfig(filename="LogFile.log",
+#                     filemode='a',
+#                     format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
+#                     datefmt='%H:%M:%S',
+#                     level=logging.DEBUG)
+
 driver = webdriver.Firefox()
+driver.get("http://www.google.com")
 tabs = 1
+
 f = open('Lists.JSON')
 lists = json.load(f)
 curr_url = "about:blank"
 
-print("Working...")
+logging.info("Program is starting up...")
 
 reader = Reader()
-print("Reader Created")
+reader_thread = threading.Thread(target=reader.start, name="RR")
+reader_thread.start()
 
 while True:
     try:
         time.sleep(1)
         url = driver.current_url
         if url != curr_url:
-            if reader.get_status():
-                reader.change_status(False)
-            print("URL Changed: " + url)
+            logging.info("URL Changed: " + url)
             for i in lists['bl']:
                 if i in url:
-                    print("[ERR] Black List URL, redirecting...")
+                    logging.error("Black List URL, redirecting...")
                     driver.back()
                     break
             else:
+                if reader.get_status():
+                    reader.change_status(False)
                 curr_url = url
                 for i in lists['wl']:
                     if i in url:
-                        print("[OK] White List URL")
+                        logging.info("White List URL")
                         break
                 else:
-                    print("[WRNG] Not in Lists, monitoring...")
+                    logging.warning("Not in Lists, monitoring...")
                     reader.change_status(True)
-                    reader.start()
-
 
         # print(driver.window_handles)
         # if tabs != len(driver.window_handles):
-            # print("Urls list:")
-            # driver.execute_script("return document.visibilityState") == "visible"
-            # temp = driver.current_window_handle
-            # print("Change")
-            # print(temp)
-            # for handle in driver.window_handles:
-            #     driver.switch_to.window(handle)
-            #     print(driver.current_url)
-            # tabs = len(driver.window_handles)
+        # print("Urls list:")
+        # driver.execute_script("return document.visibilityState") == "visible"
+        # temp = driver.current_window_handle
+        # print("Change")
+        # print(temp)
+        # for handle in driver.window_handles:
+        #     driver.switch_to.window(handle)
+        #     print(driver.current_url)
+        # tabs = len(driver.window_handles)
         #
         # print("Current data:")
         # print(driver.current_url)
@@ -58,5 +74,11 @@ while True:
         # driver.switch_to(driver.current_window_handle)
 
     except:
-        main_page = driver.window_handles[0]
-        driver.switch_to.window(main_page)
+        try:
+            main_page = driver.window_handles[0]
+            driver.switch_to.window(main_page)
+
+        except:
+            print("Browser closed")
+            reader.stop()
+            exit(0)
--- read_screen.py
+++ read_screen.py
@@ -2,6 +2,7 @@ import time
 import mss
 import numpy
 import pytesseract
+import logging
 
 pytesseract.pytesseract.tesseract_cmd = 'C:\Program Files\Tesseract-OCR\\tesseract.exe'
 mon = {'top': 0, 'left': 0, 'width': 1920, 'height': 1080}
@@ -12,6 +13,7 @@ words = f.readlines()
 
 class Reader:
     state = False
+    stopping = False
 
     def change_status(self, state):
         if state != self.state:
@@ -21,16 +23,31 @@ class Reader:
         return self.state
 
     def start(self):
-        with mss.mss() as sct:
-            while self.state:
-                im = numpy.asarray(sct.grab(mon))
+        print("Scanner is waiting")
+        while not self.state:
+            if self.stopping:
+                print("Scanner stopped")
+                return
+        try:
+            print("Start scanning...")
+            with mss.mss() as sct:
+                while self.state:
+                    if self.stopping:
+                        print("Scanner stopped")
+                        return
+                    im = numpy.asarray(sct.grab(mon))
 
-                text = pytesseract.image_to_string(im, lang="rus")
+                    text = pytesseract.image_to_string(im, lang="rus")
 
-                for i in words:
-                    if i in text:
-                        print("[WRNNG] Suspicious word:" + i)
+                    for i in words:
+                        if i in text:
+                            logging.warning("Suspicious word: " + i)
 
-                time.sleep(5)
+                    time.sleep(5)
+                self.start()
+        except Exception as err:
+            return "[ERR]" + str(err)
 
-            print("Stop Working...")
+    def stop(self) -> None:
+        if not self.stopping:
+            self.stopping = not self.stopping
